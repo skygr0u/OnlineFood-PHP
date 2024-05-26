fd887557a5af8782030b260fbecde9699b7794e7
skygr0u <azizmejri939@gmail.com>
2024-05-26 11:25:45 +0100

    final commit

---
+++ connection/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "onlinefoodphp";  
+
+
+    $db = mysqli_connect($servername, $username, $password, $dbname); 
+
+    if (!$db) {       
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+?>
